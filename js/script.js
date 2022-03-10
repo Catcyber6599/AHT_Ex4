f6dc49cea8a9602d1b47e1ab099822fb90961eae
Hieu Degea <hieudegea@gmail.com>
2022-03-10 17:30:05 +0700

    Update Code New

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ $(".detail-content").slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 1,
+        slidesToScroll: 1,
         infinite: true,
         dots: true,
       },
@@ -16,8 +16,8 @@ $(".detail-content").slick({
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToShow: 1,
+        slidesToScroll: 1,
       },
     },
     {
